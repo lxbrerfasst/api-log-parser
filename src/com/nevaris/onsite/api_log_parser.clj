b0f0d38e36d2282d40e815deae04f7f7630f0d86
Alex Bauer <ab@123erfasst.de>
2020-08-01 20:46:42 +0200

    various changes

--- src/com/nevaris/onsite/api_log_parser.clj
+++ src/com/nevaris/onsite/api_log_parser.clj
@@ -2,12 +2,10 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as string])
   (:import java.time.LocalDateTime
-           java.time.format.DateTimeFormatter
-           java.util.zip.GZIPOutputStream))
+           java.time.format.DateTimeFormatter))
 
-(comment
-  (set! *warn-on-reflection* true)
-  (set! *unchecked-math* :warn-on-boxed))
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
 
 (defn log-reader
   [file]
@@ -20,6 +18,9 @@
           (if line
             (if (and (< 36 (.length line))
                      (= \- (.charAt line 8))
+                     (= \- (.charAt line 13))
+                     (= \- (.charAt line 18))
+                     (= \- (.charAt line 23))
                      (= \; (.charAt line 36)))
               (if (zero? (.length sb))
                 (recur lines (.append sb line) acc)
@@ -30,6 +31,68 @@
                     (recur lines (.append sb line) new-acc))))
               (recur lines (-> sb (.append "\n") (.append line)) acc))
             (f acc (str sb))))))))
+
+(defn log-reader2
+  [file]
+  (reify clojure.lang.IReduceInit
+    (reduce [this f start]
+      (with-open [^java.io.BufferedReader
+                  rdr (io/reader file :encoding "ISO-8859-1")]
+        (loop [sb (StringBuilder.)
+               acc start]
+          (if-some [line (.readLine rdr)]
+            (if (and (< 36 (.length line))
+                     (= \- (.charAt line 8))
+                     (= \- (.charAt line 13))
+                     (= \- (.charAt line 18))
+                     (= \- (.charAt line 23))
+                     (= \; (.charAt line 36)))
+              (if (zero? (.length sb))
+                (recur (.append sb line) acc)
+                (let [new-acc (f acc (str sb))]
+                  (.setLength sb 0)
+                  (if (reduced? new-acc)
+                    @new-acc
+                    (recur (.append sb line) new-acc))))
+              (recur (-> sb (.append "\n") (.append line)) acc))
+            (f acc (str sb))))))))
+
+(defrecord LogEntry [ident customer-id http-status timestamp
+                     process-duration login path url user-agent
+                     http-verb ip payload payload-size exception])
+
+(def DATETIME_PATTERN (DateTimeFormatter/ofPattern "dd.MM.yyyy HH:mm:ss"))
+
+(defn parse-timestamp
+  [string]
+  (LocalDateTime/parse string DATETIME_PATTERN))
+
+(defn ip-address?
+  [^String string]
+  (let [len (.length string)
+        i (.lastIndexOf string ".")]
+    (if (and (< (- len 5) i)
+             (< i (dec len)))
+      (let [j (.lastIndexOf string "." (dec i))]
+        (if (and (< (- i 5) j)
+                 (< j (dec i)))
+          (let [k (.lastIndexOf string "." (dec j))]
+            (if (and (< (- j 5) k)
+                     (< k (dec j)))
+              (let [semicolon (.lastIndexOf string ";" (dec k))]
+                (and (< (- k 5) semicolon)
+                     (< semicolon (dec k))))
+              false))
+          false))
+      false)))
+#_
+(let []
+  (time
+   (doseq [x (repeat 1e6 ";123.123.123.123")]
+     (boolean (re-find #"\d{1,3}(?:\.\d{1,3}){3}$" x))))
+  (time
+   (doseq [x (repeat 1e6 ";123.123.123.123")]
+     (ip-address? x))))
 
 (defn parse-log-entry
   [^String entry]
@@ -44,7 +107,7 @@
         y (.lastIndexOf entry "';" z)
         ^int
         x (loop [i (.lastIndexOf entry ";'" (dec y))]
-            (if (re-find #"\d+(\.\d+){3}$" (subs entry (- i 15) i))
+            (if (ip-address? (subs entry (- i 15) i))
               i
               (recur (.lastIndexOf entry ";'" (dec i)))))
         w (.lastIndexOf entry ";" (dec x))
@@ -54,108 +117,36 @@
             (if (= \space (.charAt entry (inc i)))
               (recur (.lastIndexOf entry ";" (dec i)))
               i))]
-    [(subs entry 0 a)       ;; ident
-     (subs entry (inc a) b) ;; customer_id
-     (subs entry (inc b) c) ;; http_status
-     (subs entry (inc c) d) ;; timestamp
-     (subs entry (inc d) e) ;; process_duration
-     (subs entry (inc e) f) ;; login
-     (subs entry (inc f) g) ;; path
-     (subs entry (inc g) u) ;; url
-     (subs entry (inc u) v) ;; user_agent
-     (subs entry (inc v) w) ;; http_verb
-     (subs entry (inc w) x) ;; ip
-     (subs entry (+ 2 x) y) ;; payload
-     (subs entry (+ 2 y) z) ;; payload_size
+    (LogEntry.
+     (subs entry 0 a)           ;; ident
+     (subs entry (inc a) b)     ;; customer_id
+     (subs entry (inc b) c)     ;; http_status
+     (-> (subs entry (inc c) d) ;; timestamp
+         (parse-timestamp)
+         (str))
+     (if (== (inc d) e)
+       0.0
+       (-> (subs entry (inc d) e) ;; process_duration
+           (Double/parseDouble)))
+     (subs entry (inc e) f)     ;; login
+     (subs entry (inc f) g)     ;; path
+     (subs entry (inc g) u)     ;; url
+     (subs entry (inc u) v)     ;; user_agent
+     (subs entry (inc v) w)     ;; http_verb
+     (subs entry (inc w) x)     ;; ip
+     (subs entry (+ 2 x) y)     ;; payload
+     (-> (subs entry (+ 2 y) z) ;; payload_size
+         (Long/parseLong))
      ;; exception
-     (subs entry (+ 2 z) (dec (.length entry)))]))
+     (subs entry (+ 2 z) (dec (.length entry))))))
 
-(def DATETIME_PATTERN (DateTimeFormatter/ofPattern "dd.MM.yyyy HH:mm:ss"))
-
-(defn parse-timestamp
-  [string]
-  (LocalDateTime/parse string DATETIME_PATTERN))
-
-(defn escape-string
-  [string]
-  (-> string
-      (string/replace "\\" "\\\\")
-      (string/replace "\b" "\\b")
-      (string/replace "\f" "\\f")
-      (string/replace "\n" "\\n")
-      (string/replace "\r" "\\r")
-      (string/replace "\t" "\\t")
-      (string/replace "\"" "\\\"")))
-
-(defn format-as-json
-  [entry]
-  (let [it (.iterator ^Iterable entry)
-        sb (StringBuilder.)]
-    (.append sb "{\"ident\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"customer_id\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"http_status\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"timestamp\":\"")
-    (.append sb (str (parse-timestamp (.next it))))
-    (let [proc-time (.next it)]
-      (.append sb "\",\"process_duration\":")
-      (.append sb (if (= "" proc-time) "0" proc-time)))
-    (.append sb ",\"login\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"path\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"url\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"user_agent\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"http_verb\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"ip\":\"")
-    (.append sb (.next it))
-    (.append sb "\",\"payload\":\"")
-    (.append sb (escape-string (.next it)))
-    (.append sb "\",\"payload_size\":")
-    (.append sb (.next it))
-    (.append sb ",\"exception\":\"")
-    (.append sb (escape-string (.next it)))
-    (.append sb "\"}\n")
-    (str sb)))
+(defn read-logs*
+  [file]
+  (eduction (map parse-log-entry) (log-reader2 file)))
 
 (defn read-logs
   [file]
-  (let [ks [:ident
-            :customer-id
-            :http-status
-            :timestamp
-            :process-duration
-            :login
-            :path
-            :url
-            :user-agent
-            :http-verb
-            :ip
-            :payload
-            :payload-size
-            :exception]]
-    (into [] (comp (map parse-log-entry)
-                   (map #(zipmap ks %))
-                   (map (fn [data]
-                          (-> data
-                              (update :timestamp (comp str parse-timestamp))
-                              (update :payload-size #(Long/parseLong %))
-                              (update :process-duration #(if (= "" %) 0 (Double/parseDouble %)))))))
-          (log-reader file))))
-
-(defn -main
-  [& args]
-  (let [[in out gzip?] args]
-    (with-open [os (cond-> (io/output-stream out)
-                     gzip? (GZIPOutputStream.))
-                writer (io/writer os)]
-      (->> (log-reader in)
-           (eduction
-            (map parse-log-entry)
-            (map format-as-json))
-           (run! #(.write writer (str %)))))))
+  (try
+    (into [] (read-logs* file))
+    (catch Throwable t
+      (throw (ex-info "Failed parsing logs" {:file file} t)))))
